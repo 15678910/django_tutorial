937e56f6ab732d971bd5b94767299207087838ba
Lee soo chong <lacoiffure@hanmail.net>
2024-01-16 12:03:19 +0900

    good

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.httpfrom django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.httpfrom django.shortcuts import render
+
+def index(request)
+    return HttpResponse("Hello, world. You're at the polls index.")
+
